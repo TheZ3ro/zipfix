f1cece72805f3d99a064c08ac133a96f43a9ed81
tomasalias <80042910+tomasalias@users.noreply.github.com>
2023-04-27 18:37:18 +0200

    Update zipfix.py

--- zipfix.py
+++ zipfix.py
@@ -21,9 +21,8 @@ def write_data(filename, data):
     if filename.endswith(b'/'):
         os.mkdir(filename)
     else:
-        f = open(filename, 'wb')
-        f.write(data)
-        f.close()
+        with open(filename, 'wb') as f:
+            f.write(data)
 
 def fdescriptor_reader(file, initial_offset=zipfile.sizeFileHeader):
     offset = initial_offset
@@ -31,7 +30,7 @@ def fdescriptor_reader(file, initial_offset=zipfile.sizeFileHeader):
     while True:
         temp = file.read(1024)
         if len(temp) < 1024:
-            print('Found end of file.  Some entries missed.')
+            print('Found end of file. Some entries missed.')
             return None
 
         parts = temp.split(stringDataDescriptor)
@@ -44,7 +43,7 @@ def fdescriptor_reader(file, initial_offset=zipfile.sizeFileHeader):
 
     ddescriptor = file.read(sizeDataDescriptor)
     if len(ddescriptor) < sizeDataDescriptor:
-        print('Found end of file.  Some entries missed.')
+        print('Found end of file. Some entries missed.')
         return None
     
     ddescriptor = struct.unpack(structDataDescriptor, ddescriptor)
@@ -63,75 +62,72 @@ def main(filename):
     with zipfile.ZipFile(filename, 'r') as myzip:
         files = myzip.namelist()
         print('Found %d file(s) from Central Directory:' % (len(files)))
-        print('- '+'\n- '.join(files))
+        print('- ' + '\n- '.join(files))
 
-    print('Reading %s ZIP entry manually' % sys.argv[1])
-    f = open(filename, 'rb')
-
-    while True:
-        # Read and parse a file header
-        fheader = f.read(zipfile.sizeFileHeader)
-        if len(fheader) < zipfile.sizeFileHeader:
-            print('Found end of file.  Some entries missed.')
-            break
-        
-        fheader = struct.unpack(zipfile.structFileHeader, fheader)
-        if fheader[zipfile._FH_SIGNATURE] == zipfile.stringCentralDir:
-            print('Found start of central directory.  All entries processed.')
-            break
-
-        if fheader[zipfile._FH_SIGNATURE] != zipfile.stringFileHeader:
-            raise Exception('Size mismatch! File Header expected, got "%s"' % (fheader[zipfile._FH_SIGNATURE]))
-
-        if fheader[zipfile._FH_GENERAL_PURPOSE_FLAG_BITS] & 0x8:
-            data_descriptor = True
-        
-        fname = f.read(fheader[zipfile._FH_FILENAME_LENGTH])
-        if fheader[zipfile._FH_EXTRA_FIELD_LENGTH]:
-            f.read(fheader[zipfile._FH_EXTRA_FIELD_LENGTH])
-        print('Found %s' % fname.decode())
-
-        # Fake a zipinfo record
-        zi = zipfile.ZipInfo()
-        zi.filename = fname
-        zi.compress_size = fheader[zipfile._FH_COMPRESSED_SIZE]
-        zi.compress_type = fheader[zipfile._FH_COMPRESSION_METHOD]
-        zi.flag_bits = fheader[zipfile._FH_GENERAL_PURPOSE_FLAG_BITS]
-        zi.file_size = fheader[zipfile._FH_UNCOMPRESSED_SIZE]
-        zi.CRC = fheader[zipfile._FH_CRC]
-
-        if data_descriptor:
-            # Compress size is zero
-            # Get the real sizes with datadescriptor
-            ddescriptor = fdescriptor_reader(f, f.tell())
-            if ddescriptor is None:
+    print('Reading %s ZIP entry manually' % filename)
+    with open(filename, 'rb') as f:
+        while True:
+            # Read and parse a file header
+            fheader = f.read(zipfile.sizeFileHeader)
+            if len(fheader) < zipfile.sizeFileHeader:
+                print('Found end of file. Some entries missed.')
                 break
-            zi.compress_size = ddescriptor[_DD_COMPRESSED_SIZE]
-            zi.file_size = ddescriptor[_DD_UNCOMPRESSED_SIZE]
-            zi.CRC = ddescriptor[_DD_CRC]
-
-        #print(zi)
-
-        # Read the file contents
-        zef = zipfile.ZipExtFile(f, 'rb', zi)
-        data = zef.read()
         
-        # Sanity checks
-        if len(data) != zi.file_size:
-            raise Exception("Unzipped data doesn't match expected size! %d != %d, in %s" % (len(data), zi.file_size, fname))
-        calc_crc = zipfile.crc32(data) & 0xffffffff
-        if calc_crc != zi.CRC:
-            raise Exception('CRC mismatch! %d != %d, in %s' % (calc_crc, zi.CRC, fname))
-        
-        # Write the file
-        write_data(fname, data)
+            fheader = struct.unpack(zipfile.structFileHeader, fheader)
+            if fheader[zipfile._FH_SIGNATURE] == zipfile.stringCentralDir:
+                print('Found start of central directory. All entries processed.')
+                break
 
-        if data_descriptor:
-            # skip dataDescriptor before reading the next file
-            f.seek(f.tell()+sizeDataDescriptor)
+            if fheader[zipfile._FH_SIGNATURE] != zipfile.stringFileHeader:
+                raise Exception('Size mismatch! File Header expected, got "%s"' % (fheader[zipfile._FH_SIGNATURE]))
+
+            if fheader[zipfile._FH_GENERAL_PURPOSE_FLAG_BITS] & 0x8:
+                data_descriptor = True
+        
+            fname = f.read(fheader[zipfile._FH_FILENAME_LENGTH])
+            if fheader[zipfile._FH_EXTRA_FIELD_LENGTH]:
+                f.read(fheader[zipfile._FH_EXTRA_FIELD_LENGTH])
+            print('Found %s' % fname.decode())
+
+            # Fake a zipinfo record
+            zi = zipfile.ZipInfo()
+            zi.filename = fname
+            zi.compress_size = fheader[zipfile._FH_COMPRESSED_SIZE]
+            zi.compress_type = fheader[zipfile._FH_COMPRESSION_METHOD]
+            zi.flag_bits = fheader[zipfile._FH_GENERAL_PURPOSE_FLAG_BITS]
+
+            if data_descriptor:
+                # Compress size is zero
+                # Get the real sizes with data descriptor
+                ddescriptor = fdescriptor_reader(f, f.tell())
+                if ddescriptor is None:
+                    break
+                zi.compress_size = ddescriptor[_DD_COMPRESSED_SIZE]
+                zi.file_size = ddescriptor[_DD_UNCOMPRESSED_SIZE]
+                zi.CRC = ddescriptor[_DD_CRC]
+
+            # Read the file contents
+            zef = zipfile.ZipExtFile(f, 'rb', zi)
+            data = zef.read()
+        
+            # Sanity checks
+            if len(data) != zi.file_size:
+                raise Exception("Unzipped data doesn't match expected size! %d != %d, in %s" % (len(data), zi.file_size, fname))
+            calc_crc = zipfile.crc32(data) & 0xffffffff
+            if calc_crc != zi.CRC:
+                raise Exception('CRC mismatch! %d != %d, in %s' % (calc_crc, zi.CRC, fname))
+        
+            # Write the file
+            write_data(fname, data)
+
+            if data_descriptor:
+                # Skip dataDescriptor before reading the next file
+                f.seek(f.tell() + sizeDataDescriptor)
 
     f.close()
 
 if __name__ == '__main__':
-    main(sys.argv[1])
-
+    if len(sys.argv) < 2:
+        print("Usage: python zipfix.py <filename>")
+    else:
+        main(sys.argv[1])
